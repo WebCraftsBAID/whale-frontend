a224e72dec16dd7f612e1d70aaba3da5c986d5d2
Dreta <32196824+Dreta@users.noreply.github.com>
2024-04-02 19:54:15 +0800

    Add FontAwesome; Add base i18n

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
+        <Suspense fallback='Loading languages...'>
+
+        </Suspense>
     </React.StrictMode>,
-)
+);
