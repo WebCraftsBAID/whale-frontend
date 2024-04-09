766cd94e09dc5022f7a292bb7f1aeaf26a37d03c
Dreta <32196824+Dreta@users.noreply.github.com>
2024-04-09 21:48:36 +0800

    fix: redundant decimals for money rounding

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
 export function moneyRound(n: number): string {
     const multipli = Math.pow(10, 2)
     n = parseFloat((n * multipli).toFixed(11))
-    return (Math.round(n) / multipli).toFixed(2)
+    return (+(Math.round(n) / multipli).toFixed(2)).toString()
 }
