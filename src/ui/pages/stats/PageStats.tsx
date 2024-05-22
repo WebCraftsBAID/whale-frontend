8b39974026236fc89a469bb82ba0db1785a0ebd6
Dreta <32196824+Dreta@users.noreply.github.com>
2024-05-22 21:26:03 +0800

    chore: wip

--- src/ui/pages/stats/PageStats.tsx
+++ src/ui/pages/stats/PageStats.tsx
@@ -4,9 +4,11 @@ import { useQuery } from '@tanstack/react-query'
 import { getStats } from '../../../data/api.ts'
 import ComponentError from '../../common/ComponentError.tsx'
 import ComponentLoading from '../../common/ComponentLoading.tsx'
+import { useTranslation } from 'react-i18next'
 
 export default function PageStats(): JSX.Element {
     const persistentStorage: PersistentStorage = usePersistentStorage()
+    const { t } = useTranslation()
 
     const stats = useQuery({
         queryKey: ['stats'],
@@ -20,9 +22,10 @@ export default function PageStats(): JSX.Element {
     if (stats.isPending) {
         return <ComponentLoading screen={true} />
     }
-    console.log(stats.data)
 
     return <AnimatedPage>
-        ...
+        <div className='h-screen w-screen p-12 flex flex-col'>
+            <p className='font-display text-lg mb-5 flex-shrink'>{t('stats.title')}</p>
+        </div>
     </AnimatedPage>
 }
