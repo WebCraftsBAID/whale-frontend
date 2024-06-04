c970c5bef349d5ba56ac7dbefe847ce89151fcc6
Dreta <32196824+Dreta@users.noreply.github.com>
2024-06-04 21:07:11 +0800

    feat: add testing mode banner

--- src/AnimatedPage.tsx
+++ src/AnimatedPage.tsx
@@ -1,11 +1,21 @@
 import { motion } from 'framer-motion'
 import { type ReactNode } from 'react'
+import { useTranslation } from 'react-i18next'
 
 export default function AnimatedPage({ children }: { children: ReactNode }): JSX.Element {
+    const { t } = useTranslation()
     return (
         <motion.main
             className='main-animation'>
             {children}
+            {import.meta.env.VITE_TESTING === '1'
+                ? (
+                    <div
+                        className='absolute z-50 top-0 lg:top-[initial] lg:bottom-0 w-screen p-1 text-white font-bold bg-accent-red text-center'>
+                        {t('a11y.testing')}
+                    </div>
+                )
+                : null}
         </motion.main>
     )
 }
